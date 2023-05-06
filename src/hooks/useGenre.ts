8d236af0fc23cd934db335dfe415739acd85acb1
ask4darshini <ask4darshini@gmail.com>
2023-05-06 23:09:24 +0530

    Refactor Hook

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre{
+export interface Genre{
  id: number;
  name: string;
 }
 
-interface GenreResponse{
-    count: number;
-    results: Genre[];
+const useGenre = () => useData<Genre>('./genres');
 
-}
-const useGenre = () => {
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      const controller = new AbortController();
-     
-     setLoading(true);
-  
-      apiClient
-        .get<GenreResponse>("/genres")
-        .then((res) => {
-          setGenres(res.data.results)
-          setLoading(false)
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        });
-  
-      return () => controller.abort();
-    }, []);
-  
-    return { genres, error, isLoading };
-}
 
 export default useGenre;
