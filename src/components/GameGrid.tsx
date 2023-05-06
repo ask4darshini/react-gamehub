5a9a836dff46f2e217b6663b31f2b0fccc9fae97
ask4darshini <ask4darshini@gmail.com>
2023-05-06 14:58:52 +0530

    Passing Props to Child Component GameCard

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useFetch from "../hooks/useFetch";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
     const {games, error} = useFetch()
@@ -7,10 +8,12 @@ const GameGrid = () => {
   return (
     <>
     {error && <Text>{error}</Text>}
-   <ul>
-    {games.map( (game) => {
-    return <li key={game.id}> {game.name} </li> })}
-   </ul>
+    <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} spacing={10}>
+    {games.map( game => 
+      <GameCard key={game.id} game={game}/>
+      )}
+
+  </SimpleGrid>
    </>
   );
 };
