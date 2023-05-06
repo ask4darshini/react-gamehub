e691876c0bf9ce6aac12235d62ff6a0731b5cc13
ask4darshini <ask4darshini@gmail.com>
2023-05-06 14:35:20 +0530

    custom hook useFetch used

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  interface GamesResponse {
+    count: number;
+    results: Game[];
+  }
+  
+const useFetch = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+      apiClient
+        .get<GamesResponse>("/xgames")
+        .then((res) => setGames(res.data.results))
+        .catch((err) => setError(err.message));
+    });
+
+    return {games, error};
+}
+
+export default useFetch;
