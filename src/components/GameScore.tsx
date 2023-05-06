552f546c8bb01554ec26c9ce9a6e4f3add89ddea
ask4darshini <ask4darshini@gmail.com>
2023-05-06 18:13:36 +0530

    Conditional Styling.

--- src/components/GameScore.tsx
+++ src/components/GameScore.tsx
@@ -4,8 +4,10 @@ interface Props{
     score: number;
 }
 const GameScore = ({score}: Props) => {
+
+    let color = score > 75? 'green' : score > 60 ? 'yellow' : '';
   return (
-   <Badge fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
+   <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
   )
 }
 
