4e7e290947432e2af5e16d9ad0b417514a7740a9
ask4darshini <ask4darshini@gmail.com>
2023-05-06 16:30:06 +0530

    Render Icon

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import {Text} from '@chakra-ui/react';
-import {Platform} from '../hooks/useFetch';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import {MdPhoneIphone} from 'react-icons/md';
+import {SiNintendo} from 'react-icons/si';
+import {BsGlobe} from 'react-icons/bs'
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useFetch";
+import { IconType } from "react-icons";
 
 interface Props {
-    platforms: Platform[]
+  platforms: Platform[];
 }
-const PlatformIconList = ({platforms} : Props) => {
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap : {[key: string]: IconType} = {
+        //name: PlayStation
+        //slug: Playstation
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
   return (
-  <> 
-                  {platforms.map((platform) => <Text>{platform.name}</Text>)}
+    <HStack>
+      {platforms.map((platform) => (
+       <Icon as={iconMap[platform.slug]} />
+      ))}
+    </HStack>
+  );
+};
 
-                  </>
-  )
-}
-
-export default PlatformIconList
+export default PlatformIconList;
