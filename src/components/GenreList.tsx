4a3b67e43df0b60faed8b7b5044bdff205250018
ask4darshini <ask4darshini@gmail.com>
2023-05-06 23:35:10 +0530

    Add Image To SidePanel

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,24 @@
-import useGenre from '../hooks/useGenre';
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenre from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const {data} = useGenre();
+  const { data } = useGenre();
   return (
-    <ul>
-        {data.map(genre => <li key={genre.id}>{genre.name} </li>)}
-    </ul>
-
-  )
-}
-export default GenreList
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+export default GenreList;
